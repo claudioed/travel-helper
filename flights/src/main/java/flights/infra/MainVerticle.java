b2c853654692e6980580b76b86eee2f1a7195aa2
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2017-07-07 23:43:53 -0300

    cluster manager for projects

---
+++ flights/src/main/java/flights/infra/MainVerticle.java
@@ -0,0 +1,24 @@
+package flights.infra;
+
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.Vertx;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+/**
+ * Flights main verticle
+ * @author claudioed on 07/07/17. Project travel-helper
+ */
+public class MainVerticle extends AbstractVerticle{
+
+  @Override
+  public void start() throws Exception {
+    ClusterManager mgr = new HazelcastClusterManager();
+    VertxOptions options = new VertxOptions().setClusterManager(mgr);
+    Vertx.clusteredVertx(options, res -> {
+      final Vertx vertx = res.result();
+      vertx.deployVerticle(RequestFlightsVerticle.class.getName());
+    });
+  }
+}
---
+++ points/src/main/java/points/infra/MainVerticle.java
@@ -0,0 +1,25 @@
+package points.infra;
+
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.Vertx;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+/**
+ * Main point verticle
+ * @author claudioed on 07/07/17. Project travel-helper
+ */
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() throws Exception {
+    ClusterManager mgr = new HazelcastClusterManager();
+    VertxOptions options = new VertxOptions().setClusterManager(mgr);
+    Vertx.clusteredVertx(options, res -> {
+      final Vertx vertx = res.result();
+      vertx.deployVerticle(RequestPointsVerticle.class.getName());
+    });
+  }
+
+}
